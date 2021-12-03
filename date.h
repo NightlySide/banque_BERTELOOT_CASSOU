f2c1a8d51b00a0d5af2cc9a9b47e5e7ccf64adda
EmileBrt <emile.berteloot@ensta-bretagne.org>
2021-12-03 16:40:35 -0500

    let's go

---
+++ date.h
@@ -0,0 +1,14 @@
+//
+// Created by Emile on 03/12/2021.
+//
+
+#ifndef PROJET_BANQUE_DATE_H
+#define PROJET_BANQUE_DATE_H
+#endif //PROJET_BANQUE_DATE_H
+#include "time.h"
+struct date
+{
+    int jour;
+    int mois;
+    int annee;
+};
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "date.h"
+
+void now_date( struct date){
+    time_t timestamp = time(NULL);
+    struct tm * now = localtime( &timestamp);
+    int jour = now->tm_mday;
+    int mois = now ->tm_mon;
+    int annee = now ->tm_year;
+}
+int main(int argc, char * argv[]) {
+    struct date vendr;
+    now_date(vendr);
+    printf(vendr.annee);
+    printf(vendr.mois);
+    return 1;
+}
