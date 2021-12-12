45aac472833a765c97f662543a475a5f57930f3a
emile <emile.berteloot@ensta-bretagne.org>
2021-12-12 05:40:35 -0500

    yes

--- date.h
+++ date.h
@@ -1,9 +1,13 @@
 #include "time.h"
 
+#ifndef DATE_H
+#define DATE_H
+
 typedef struct {
     int jour;
     int mois;
     int annee;
 } DATE;
 
+#endif
 //void now_date(DATE *date_j);
--- entete.h
+++ entete.h
@@ -1,5 +1,12 @@
 #include "date.h"
+
+
+#ifndef ENTETE_H
+#define ENTETE_H
+
 typedef struct {
     DATE dateActuelle;
     float solde;
 } ENTETE;
+
+#endif
